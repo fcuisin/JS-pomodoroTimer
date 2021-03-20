52a6dcb39fa233cc7e97fab6269c328958595ca1
Ferdinand Cuisin <fcuisin@gmail.com>
2021-03-20 15:08:33 +0100

    Add delete option for any action

--- script.js
+++ script.js
@@ -172,14 +172,32 @@ function createAction() {
 
 function populateList() {
 
-  list.innerHTML = listOfActions.map(action => {
+  list.innerHTML = listOfActions.map((action, index) => {
      return `
-      <div class="action-item">
+      <div data-key=${index} class="action-item">
         <li>${action.name} - ${action.time}</li>
         <button class="delete-action"><i class="far fa-trash-alt"></i></button>
       </div>
     `
   }).join('');
+
+  const itemsToDelete = document.querySelectorAll(".delete-action");
+  itemsToDelete.forEach( item => {
+
+    item.addEventListener("click", function() {
+      return deleteAction(this.parentNode.dataset.key)
+    });
+
+  });
+
+}
+
+function deleteAction(id) {
+
+  listOfActions.splice(id, 1);
+  localStorage.setItem('items', JSON.stringify(listOfActions));
+
+  return populateList();
 
 }
 
